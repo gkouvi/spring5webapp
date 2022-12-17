9a01a038de1f5f04d716fddfb553df0db3b932e4
Kostas Gkouvas <gkouvi@gmail.com>
2022-12-17 22:49:20 +0200

    before adding data after publisher

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootsSrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootsSrapData.java
@@ -1,7 +1,11 @@
 package guru.springframework.spring5webapp.bootstrap;
 
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -10,14 +14,36 @@ public class BootsSrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootsSrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootsSrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository=publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        Author eric = new Author("eric","Adams");
+        Book ddd = new Book("Domain Driven Design","123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
 
+        Author rod = new Author("Rod","Johnson");
+        Book noEJB = new Book("noEJB forjava developers","123456789");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        Publisher publisher = new Publisher("EXER","Ioannina,home,2,34");
+        publisherRepository.save(publisher);
+
+
+        System.out.println("Bootstrap started");
+        System.out.println("Number of books "+bookRepository.count());
+        System.out.println("Number of Available publishers: "+publisherRepository.count());
     }
 }
